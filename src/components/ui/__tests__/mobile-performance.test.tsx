068a897c9abb53ebffc2073f833567f5f2cbfec2
Luke Taylor <luke@dibza.co.uk>
2025-12-10 18:33:00 +0000

    eslint errors

--- src/components/ui/__tests__/mobile-performance.test.tsx
+++ src/components/ui/__tests__/mobile-performance.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import TouchButton from "../touch-button";
 import { mobilePerformanceUtils } from "@/lib/mobile-performance";
